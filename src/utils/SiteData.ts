9abb8e87223020cb5583ecfa87a50c32edbb37c1
ted <ted@warpedpuppy.com>
2024-08-10 18:26:17 -0400

    converted sitedata to typescript

--- src/utils/SiteData.ts
+++ src/utils/SiteData.ts
@@ -1,10 +1,17 @@
+interface LooseObject {
+  [key: string]: any
+}
+type Point = {
+  x: number;
+  y:number;
+}
 const SiteData = {
   trig: {
     circleFromThreePoints: {
       t: "get circle from three points",
       l: "circle-from-three-points",
-      bf: function (canvas) {
-        return {
+      bf: function (canvas: HTMLCanvasElement) {
+        const obj: LooseObject = {
           text: "",
           points: [],
           init() {
@@ -12,7 +19,7 @@ const SiteData = {
             this.ctx = canvas.getContext("2d");
             canvas.addEventListener("pointerdown", this.pointerDownHandler.bind(this))
           },
-          pointerDownHandler(e) {
+          pointerDownHandler(e: PointerEvent) {
             this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
             let { top, left } = this.canvas.getBoundingClientRect();
             if (this.points.length === 3) this.points.shift();
@@ -21,7 +28,7 @@ const SiteData = {
             this.ctx.fillText("points:", 10, 30);
             this.ctx.fillText(this.formatText(), 10, 50);
 
-            this.points.forEach(item => {
+            this.points.forEach((item:Array<Array<Number>>) => {
               this.ctx.beginPath();
               this.ctx.arc(item[0], item[1], 5, 0, 2 * Math.PI);
               this.ctx.stroke();
@@ -29,7 +36,7 @@ const SiteData = {
 
           },
           formatText() {
-            return this.points.map((item, index) => {
+            return this.points.map((item:Array<Array<number>>, index: number) => {
               let i = index + 1;
               return `point ${i}: {x:${item[0]}, y:${item[1]}}`
             }).join(",")
@@ -38,9 +45,11 @@ const SiteData = {
           stop() {
             canvas.removeEventListener("pointerdown", this.pointerDownHandler)
           }
+          
         }
+        return obj;
       },
-      f: function (x1, y1, x2, y2, x3, y3) {
+      f: function (x1: number, y1: number, x2: number, y2: number, x3: number, y3: number) {
         var x12 = (x1 - x2);
         var x13 = (x1 - x3);
 
@@ -95,20 +104,20 @@ const SiteData = {
     getHalfwayPointofLine: {
       t: "get halfway point in line",
       l: "halfway-point-in-line",
-      f: function (start, end) {
+      f: function (start: Point, end: Point) {
         return {
           x: (start.x + end.x) / 2,
           y: (start.y + end.y) / 2
         }
       },
-      bf: function (canvas) {
-        return {
+      bf: function (canvas: HTMLCanvasElement) {
+        const obj: LooseObject = {
           init() {
             this.canvas = canvas;
             const ctx = canvas.getContext("2d");
             canvas.addEventListener("pointerdown", this.pointerDownHandler)
           },
-          pointerDownHandler(e) {
+          pointerDownHandler(e: PointerEvent) {
             console.log(e, "halfway")
 
           },
@@ -116,25 +125,26 @@ const SiteData = {
             canvas.removeEventListener("pointerdown", this.pointerDownHandler)
           }
         }
+        return obj;
       }
     },
     equilateralTriangleVertices: {
       t: "get equilateral triangle vertices from radius and center point",
       l: "equilateral-trianlge-points",
-      f: function (radius, centerPoint) {
+      f: function (radius: number, centerPoint: Point) {
         let point1 = { x: radius * Math.cos(0) + centerPoint.x, y: radius * Math.sin(0) + centerPoint.y }
         let point2 = { x: radius * Math.cos((1 / 3) * (2 * Math.PI)) + centerPoint.x, y: radius * Math.sin((1 / 3) * (2 * Math.PI)) + centerPoint.y }
         let point3 = { x: radius * Math.cos((2 / 3) * (2 * Math.PI)) + centerPoint.x, y: radius * Math.sin((2 / 3) * (2 * Math.PI)) + centerPoint.y }
         return { point1, point2, point3 }
       },
-      bf: function (canvas) {
-        return {
+      bf: function (canvas: HTMLCanvasElement) {
+        let obj: LooseObject = {
           init() {
             this.canvas = canvas;
             const ctx = canvas.getContext("2d");
             canvas.addEventListener("pointerdown", this.pointerDownHandler)
           },
-          pointerDownHandler(e) {
+          pointerDownHandler(e: PointerEvent) {
             console.log(e, "equilateral trianlge")
 
           },
@@ -142,6 +152,7 @@ const SiteData = {
             canvas.removeEventListener("pointerdown", this.pointerDownHandler)
           }
         }
+        return obj;
       }
     }
   }
