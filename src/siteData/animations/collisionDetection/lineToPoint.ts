37b611835f71414ea1662756d1a526c8f0b01333
ted <ted@warpedpuppy.com>
2024-08-26 10:22:12 -0400

    line point

--- src/siteData/animations/collisionDetection/lineToPoint.ts
+++ src/siteData/animations/collisionDetection/lineToPoint.ts
@@ -1,6 +1,7 @@
 import { GenericObject, Point, Line, Circle } from "../../../types/types";
 import AnimationBaseClass from "../AnimationBaseClass";
-
+import { LinePoint } from "../utils/collision-detection/LineCollision";
+import { sineCurve } from "../utils/OmnibusUtils";
 class LineToPointCollision extends AnimationBaseClass {
   static t = "line to point collision";
   static l = "line-to-point-collision";
@@ -17,18 +18,23 @@ class LineToPointCollision extends AnimationBaseClass {
     vy: 0,
     id: "circle",
   };
-  mousePoint: Point = {
+  point: Point = {
     x: 0,
     y: 0,
   };
   init() {
     this.draw();
   }
+  makePointMove() {
+    let x = sineCurve(this.halfWidth, 200, 0.001);
+    let y = sineCurve(this.halfHeight, 200, 0.001);
+    return { x, y };
+  }
   draw = () => {
     if (!this.ctx) return;
     this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
 
-    if (this.keyFunction(this.line, this.mousePoint)) {
+    if (LinePoint(this.line, this.point)) {
       this.ctx.fillStyle = "red";
       this.ctx.strokeStyle = "red";
     } else {
@@ -36,69 +42,26 @@ class LineToPointCollision extends AnimationBaseClass {
       this.ctx.strokeStyle = "black";
     }
 
+    let { x, y } = this.makePointMove();
+    this.point.x = x;
+    this.point.y = y;
+
     this.ctx.beginPath();
-    this.ctx.arc(
-      this.circle.x,
-      this.circle.y,
-      this.circle.radius,
-      0,
-      2 * Math.PI
-    );
+    this.ctx.arc(x, y, this.circle.radius, 0, 2 * Math.PI);
     this.ctx.fill();
-    this.ctx.stroke();
 
     this.ctx.lineWidth = 3;
+    this.line.startPoint = { x: this.halfWidth - 200, y: this.halfHeight };
+    this.line.endPoint = { x: this.halfWidth + 200, y: this.halfHeight };
     this.ctx.beginPath();
-    this.ctx.moveTo(this.line.startPoint.x, this.line.startPoint.y);
-    this.ctx.lineTo(this.line.endPoint.x, this.line.endPoint.y);
+    this.ctx.moveTo(this.halfWidth - 200, this.halfHeight);
+    this.ctx.lineTo(this.halfWidth + 200, this.halfHeight);
     this.ctx.stroke();
 
     requestAnimationFrame(this.draw);
   };
-  keyFunction(line: Line, point: Point) {
-    // get distance from the point to the two ends of the line
-    let d1 = this.lineLength(point, line.startPoint);
-    let d2 = this.lineLength(point, line.endPoint);
-
-    // get the length of the line
-    let lineLen = this.lineLength(line.startPoint, line.endPoint);
-
-    // since floats are so minutely accurate, add
-    // a little buffer zone that will give collision
-    let buffer = 0.1; // higher # = less accurate
-
-    // if the two distances are equal to the line's
-    // length, the point is on the line!
-    // note we use the buffer here to give a range,
-    // rather than one #
-    if (d1 + d2 >= lineLen - buffer && d1 + d2 <= lineLen + buffer) {
-      return true;
-    }
-    return false;
-  }
-  lineLength(startPoint: Point, endPoint: Point) {
-    let a = startPoint.x - endPoint.x;
-    let b = startPoint.y - endPoint.y;
-    return Math.sqrt(a * a + b * b);
-  }
-  pointerDownHandler(e: PointerEvent) {
-    // let x = e.pageX - this.left;
-    // let y = e.pageY - this.top;
-    // this.line.startPoint = { x, y };
-    // this.line.endPoint = { x, y };
-    // this.allowDraw = true;
-  }
-  pointerUpHandler(e: PointerEvent) {
-    // this.allowDraw = false;
-  }
-  pointerMoveHandler(e: PointerEvent) {
-    // if (this.allowDraw) {
-    let x = e.pageX - this.left;
-    let y = e.pageY - this.top;
-    this.circle.x = this.mousePoint.x = x;
-    this.circle.y = this.mousePoint.y = y;
-
-    // }
-  }
+  pointerDownHandler(e: PointerEvent) {}
+  pointerUpHandler(e: PointerEvent) {}
+  pointerMoveHandler(e: PointerEvent) {}
 }
 export default LineToPointCollision;
