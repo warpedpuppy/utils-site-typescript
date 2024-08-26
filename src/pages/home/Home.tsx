7beaefaa7075754b9567b5744f368cbc7b6c137a
ted <ted@warpedpuppy.com>
2024-08-26 16:37:44 -0400

    fixed star code

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -14,7 +14,19 @@ function Home() {
     <section id="home-page">
       <div id="home-page--text-container">
         <div id="home-page--text-container_inner">
-          <p>sdfgsdfgsdfg</p>
+          <p>
+            This site will help you create animations on the web by constructing
+            a custom "utils" file that will contain all of the animation
+            functions you need. Can't remember whether it is sine or cosine that
+            helps you fine the y value of a point around a circle? This site
+            will generate a function for you to calculate it. (it is the sine
+            btw -- remember the "opposite" side will be the vertical one)
+          </p>
+          <p>
+            Just go to the "generate utils" page and choose which functions you
+            want, then click the other page and you can copy and paste it into
+            your file system.
+          </p>
         </div>
       </div>
       <div id="home-page--canvas-container"></div>
