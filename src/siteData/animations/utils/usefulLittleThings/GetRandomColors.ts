1acf15a16836aa3dbedae273367a7bcd2282c825
ted <ted@warpedpuppy.com>
2024-08-28 15:24:52 -0400

    started hsl gen

--- src/siteData/animations/utils/usefulLittleThings/GetRandomColors.ts
+++ src/siteData/animations/utils/usefulLittleThings/GetRandomColors.ts
@@ -2,10 +2,19 @@ import { CollisionDetectionObject } from "../../../../types/types";
 
 export const GetRandomColors: CollisionDetectionObject = {
   keyFunction: function GetRandomColors() {
-    // Blues
-    // Hue	210° - 260°
-    // Saturation	70% - 100%
-    // Lightness	30% - 70%
+    // Hue:
+    // reds: 0 - 40
+    // orange: 40 - 80
+    // green: 80 - 150
+    //blue: 200 - 250
+    // purple: 280 - 320
+
+    // Saturation:
+    // % of color in color
+
+    // Lightness:
+    // % of white in color
+
     return {};
   },
   dependencies: [],
