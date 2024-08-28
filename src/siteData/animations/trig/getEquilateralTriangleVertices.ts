abbe21a4d7fe7aaaab4748c74aeeb02f36d754fd
ted <ted@warpedpuppy.com>
2024-08-28 11:19:07 -0400

    equilateral triangle

--- src/siteData/animations/trig/getEquilateralTriangleVertices.ts
+++ src/siteData/animations/trig/getEquilateralTriangleVertices.ts
@@ -1,5 +1,6 @@
 import { Point } from "../../../types/shapes";
-import { GenericObject } from "../../../types/types";
+import { GenericObject, Nullable } from "../../../types/types";
+import { EquilateralTriangle } from "../utils/animation/EquilateralTriangle";
 import AnimationBaseClass from "../AnimationBaseClass";
 
 class GetEquilateralTriangleVertices extends AnimationBaseClass {
@@ -7,31 +8,10 @@ class GetEquilateralTriangleVertices extends AnimationBaseClass {
   static l = "equilateral-trianlge-points";
   title = "draw equilateral triangle (from radius and center point)";
   backgroundTris: object[] = [];
+  animationObject = EquilateralTriangle;
   angle = 0;
-  keyFunction(radius: number, centerPoint: Point, angle: number) {
-    let allRadiansInACircle = 2 * Math.PI;
-    let point1 = {
-      x: radius * Math.cos(angle) + centerPoint.x,
-      y: radius * Math.sin(angle) + centerPoint.y,
-    };
-    let point2 = {
-      x:
-        radius * Math.cos(angle + (1 / 3) * allRadiansInACircle) +
-        centerPoint.x,
-      y:
-        radius * Math.sin(angle + (1 / 3) * allRadiansInACircle) +
-        centerPoint.y,
-    };
-    let point3 = {
-      x:
-        radius * Math.cos(angle + (2 / 3) * allRadiansInACircle) +
-        centerPoint.x,
-      y:
-        radius * Math.sin(angle + (2 / 3) * allRadiansInACircle) +
-        centerPoint.y,
-    };
-    return { point1, point2, point3 };
-  }
+  startPoint: Point = { x: this.halfWidth, y: this.halfHeight + 50 };
+  endPoint: Point = { x: this.halfWidth, y: this.halfHeight - 50 };
   init() {
     for (let i = 0; i < 10; i++) {
       let radius = Math.random() * 100 + 50;
@@ -59,7 +39,7 @@ class GetEquilateralTriangleVertices extends AnimationBaseClass {
 
     this.backgroundTris.forEach((tri: GenericObject) => {
       if (!this.ctx) return;
-      let { point1, point2, point3 } = this.keyFunction(
+      let { point1, point2, point3 } = EquilateralTriangle.keyFunction(
         tri.radius,
         tri.centerPoint,
         this.angle || tri.angle
@@ -86,7 +66,7 @@ class GetEquilateralTriangleVertices extends AnimationBaseClass {
 
     this.angle = this.getLineAngle(this.startPoint, this.endPoint);
 
-    let { point1, point2, point3 } = this.keyFunction(
+    let { point1, point2, point3 } = EquilateralTriangle.keyFunction(
       radius,
       this.startPoint,
       this.angle
@@ -103,8 +83,6 @@ class GetEquilateralTriangleVertices extends AnimationBaseClass {
       this.ctx?.stroke();
     });
 
-    if (this.textDiv)
-      this.textDiv.innerHTML = `<h3>Click and drag to draw equilateral triangle</h3>`;
     requestAnimationFrame(this.draw);
   };
   pointerDownHandler(e: PointerEvent) {
