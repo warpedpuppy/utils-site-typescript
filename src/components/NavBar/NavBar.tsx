df55be362169d3f70c4a26c3e4a41f9b1f13a3aa
ted <ted@warpedpuppy.com>
2024-12-30 11:01:42 -0500

    added hamburger

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,12 +1,25 @@
 import "./NavBar.scss";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 function NavBar() {
+  const [collapsed, setCollapsed] = useState(false);
+
   return (
     <nav>
-      <Link to="/">
-        <h1>utilspalooza</h1>
-      </Link>
-      <div>
+      <h1>
+        <Link to="/">utilspalooza</Link>
+      </h1>
+
+      <div
+        id="hamburger"
+        onClick={() => setCollapsed(!collapsed)}
+        className={collapsed ? "collapsed" : ""}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <div id="nav-links" className={collapsed ? "collapsed" : ""}>
         <Link to="/examples">examples</Link>
         <Link to="/create-json">create utils file</Link>
         <Link to="/about">about</Link>
