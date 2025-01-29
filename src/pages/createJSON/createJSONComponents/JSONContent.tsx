4563d1465f0646ce945158dc6c5e29fed5c27764
ted <ted@warpedpuppy.com>
2025-01-29 11:41:05 -0500

    fixed floating issue

--- src/pages/createJSON/createJSONComponents/JSONContent.tsx
+++ src/pages/createJSON/createJSONComponents/JSONContent.tsx
@@ -4,6 +4,13 @@ import "./JSONContent.scss";
 function JSONContent() {
   let formattedJSON: any = CreateJson();
 
+  if (!formattedJSON) {
+    return (
+      <div className="text-container-div">
+        <p>There are no functions here!</p>
+      </div>
+    );
+  }
   return <pre className="functions-pre">{formattedJSON}</pre>;
 }
 
